cf194630356ec17aa75dd871f4b22b192fa03174
solomoni1 <seoungjong@naver.com>
2022-12-29 19:03:35 +0900

    fix eslint and prettier configurations

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,15 @@
 const {
-    addDecoratorsLegacy,
-    disableEsLint, // eslint를 끄는 것은 아님
-    override,
-    addWebpackAlias,
-  } = require('customize-cra');
-  const path = require('path');
-  
-  module.exports = {
-    webpack: override(disableEsLint(), addDecoratorsLegacy(), addWebpackAlias({'@': path.resolve(__dirname, 'src')})),
-  };
-  
+  addDecoratorsLegacy,
+  disableEsLint, // eslint를 끄는 것은 아님
+  override,
+  addWebpackAlias,
+} = require('customize-cra');
+const path = require('path');
+
+module.exports = {
+  webpack: override(
+    disableEsLint(),
+    addDecoratorsLegacy(),
+    addWebpackAlias({ '@': path.resolve(__dirname, 'src') })
+  ),
+};
